26960be52a1da00679a1dd479960418aecf14117
simonmags <simongithub@gmail.com>
2017-08-21 13:31:55 +0930

    Updated Incoming_call
    Updated to work with the normal software serial source code from the Fona Test example. Note this still uses different pins for the incoming call example as Pin 2 is required within this sketch to notify for incoming calls etc.

--- examples/IncomingCall/IncomingCall.ino
+++ examples/IncomingCall/IncomingCall.ino
@@ -5,9 +5,9 @@
 
 // Pins which are connected to the FONA.
 // Note that this is different from FONAtest!
-#define FONA_RX            3
-#define FONA_TX            4
-#define FONA_RST           5
+#define FONA_RX 3
+#define FONA_TX 4
+#define FONA_RST 5
 
 // Note you need to map interrupt number to pin number
 // for your board.  On an Uno & Mega interrupt 0 is
@@ -17,7 +17,7 @@
 // Make sure this interrupt pin is connected to FONA RI!
 #define FONA_RI_INTERRUPT  0
 
-// We default to using software serial. If you want to use hardware serial
+/// We default to using software serial. If you want to use hardware serial
 // (because softserial isnt supported) comment out the following three lines 
 // and uncomment the HardwareSerial line
 #include <SoftwareSerial.h>
@@ -27,28 +27,62 @@ SoftwareSerial *fonaSerial = &fonaSS;
 // Hardware serial is also possible!
 //  HardwareSerial *fonaSerial = &Serial1;
 
-Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
+// Use this for FONA 800 and 808s
+//Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
+// Use this one for FONA 3G
+Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST);
+
+uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
+
+uint8_t type;
 
 void setup() {
+  while (!Serial);
+
   Serial.begin(115200);
-  Serial.println(F("FONA incoming call example"));
+  Serial.println(F("FONA basic test"));
   Serial.println(F("Initializing....(May take 3 seconds)"));
 
   fonaSerial->begin(4800);
   if (! fona.begin(*fonaSerial)) {
     Serial.println(F("Couldn't find FONA"));
-    while(1);
+    while (1);
   }
+  type = fona.type();
   Serial.println(F("FONA is OK"));
-
+  Serial.print(F("Found "));
+  switch (type) {
+    case FONA800L:
+      Serial.println(F("FONA 800L")); break;
+    case FONA800H:
+      Serial.println(F("FONA 800H")); break;
+    case FONA808_V1:
+      Serial.println(F("FONA 808 (v1)")); break;
+    case FONA808_V2:
+      Serial.println(F("FONA 808 (v2)")); break;
+    case FONA3G_A:
+      Serial.println(F("FONA 3G (American)")); break;
+    case FONA3G_E:
+      Serial.println(F("FONA 3G (European)")); break;
+    default: 
+      Serial.println(F("???")); break;
+  }
+  
+  // Print module IMEI number.
+  char imei[15] = {0}; // MUST use a 16 character buffer for IMEI!
+  uint8_t imeiLen = fona.getIMEI(imei);
+  if (imeiLen > 0) {
+    Serial.print("Module IMEI: "); Serial.println(imei);
+  }
+ 
   // Enable incoming call notification.
   if(fona.callerIdNotification(true, FONA_RI_INTERRUPT)) {
     Serial.println(F("Caller id notification enabled."));
   }
   else {
     Serial.println(F("Caller id notification disabled"));
-  }
-}
+  } //end of else for if fona.callerIdNotification
+} // end of void setup
 
 void loop(){
   // Create a small string buffer to hold incoming call number.
